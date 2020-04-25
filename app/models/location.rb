f77a17794aad27c1c7960a2f5da6a761994cf600
Khris Punzalan <khrispunzalan@gmail.com>
2020-04-24 17:57:07 -0700

    added latitude and longitude validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,18 @@
 class Location < ApplicationRecord
     has_many :adventures, dependent: :destroy
 
-    # add validations
-    # presence & uniqueness of park
     validates :park, presence: true
     validates :park, uniqueness: true
+    
+    # Latitude and Longitude validations
+    # Error on Geocoder lat >= -90 && lat <= 90
+    validates :lat , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
+    validates :long, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 end
+
+# change column type from float to decimal
+# change_column :events, :latitude, :decimal, { precision: 10, scale: 6 }
+# change_column :events, :longitude, :decimal, { precision: 10, scale: 6 }
+
+# https://github.com/andrewgr/missing_validators
+# http://www.rubygeocoder.com/
