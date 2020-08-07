ac329eda0d3b2bef01e9e368697e436865f0dc72
Khris Punzalan <khrispunzalan@gmail.com>
2020-08-06 19:54:40 -0500

    added validations & relationships on User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    # relationships
+    has_many :locations
+    has_many :adventures
+
+    validates :username, presence: true
+    validates :email, presence: true
+
+    validates :username, uniqueness: true
+    validates :email, uniqueness: true
+
 end
